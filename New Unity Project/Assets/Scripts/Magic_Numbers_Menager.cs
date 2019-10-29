451b20153f3b79f4412b042a2ea9a2889c2d911b
Łukasz Żwak <only.two.pilots@gmail.com>
2019-10-29 16:46:08 +0100

    Started with programing in C#

---
+++ New Unity Project/Assets/Scripts/Magic_Numbers_Menager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magic_Numbers_Menager : MonoBehaviour
+{
+    public int numbers=1;
+    float othernumbers=2.125f;
+    bool numberscostam = true;
+    private void Start()
+    {
+        Debug.Log($"Hello World{numbers}");
+        int Lukasz = 2;
+        Lukasz += 2;
+        Debug.Log($"{Lukasz}");
+    }
+
+
+}
